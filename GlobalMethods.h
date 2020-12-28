234a9a83e3a2d6cb10dad0f9dd8817dae0fe87e2
Ksenia <ksusha.senkevich@yandex.ru>
2020-12-28 03:47:45 +0300

    adding globalmethods.h

---
+++ GlobalMethods.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+using namespace std;
+
+// глобальные методы 
+void getaLine(string& inStr); // получение строки текста
+char getaChar(); // получение символа
+void enterProbel(); // нажатие пробела
+void color(int); //цвет текста
+int CheckInt(int i); // проверка на неотрицательность переменной типа unsigned int
+long long CheckLong(long long l); // проверка на неотрицательность переменной типа unsigned long long
